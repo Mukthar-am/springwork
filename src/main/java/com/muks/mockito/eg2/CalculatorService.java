d9298d8fc518eed26a3f0980ca82930299288404
Mukthar  Ahmed M <15692@en-muktharm.mynt.myntra.com>
2016-06-21 13:35:46 +0530

    final commit

---
+++ src/main/java/com/muks/mockito/eg2/CalculatorService.java
@@ -0,0 +1,11 @@
+package com.muks.mockito.eg2;
+
+/**
+ * Created by 15692 on 20/06/16.
+ */
+public interface CalculatorService {
+    public double add(double input1, double input2);
+    public double subtract(double input1, double input2);
+    public double multiply(double input1, double input2);
+    public double divide(double input1, double input2);
+}
